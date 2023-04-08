d42daf03cd33e0fda4aa69042dd2f8c205eeaebf
Maribel Perez Lopez <perezlopezm831@gmail.com>
2023-04-08 09:59:50 -0600

    Primer commit

---
+++ src/app/heroe/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+  heroes : string[] = ['Ironman', 'Spiderman', 'Hulk']
+  heroeDelete: string = '';
+
+  borrarHeroe(){
+    this.heroeDelete = this.heroes.pop()  || '';
+  }
+}
